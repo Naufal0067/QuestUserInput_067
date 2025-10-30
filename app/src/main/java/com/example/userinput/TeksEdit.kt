31f07a4b8f507c5aef8c26a9fb28776fb70a4c18
Naufal0067 <naufalopang1928@gmail.com>
2025-10-30 11:41:05 +0700

    membuat variabel untuk mengingat nilai dari keyboard

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,2 +1,18 @@
 package com.example.userinput
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+) {
+
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+    var
+}
