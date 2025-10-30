9c8982808dee6021331c39a766584eb6a2a89ed9
Naufal0067 <naufalopang1928@gmail.com>
2025-10-30 11:42:09 +0700

    membuat variabel untuk menyimpan data yang diperoleh

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -14,5 +14,7 @@ fun FormDataDiri(modifier: Modifier
     var textAlamat by remember { mutableStateOf("") }
     var textJK by remember { mutableStateOf("") }
 
-    var
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
 }
