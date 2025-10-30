bcd68edad8944424de521229e4887eb80bead441
Naufal0067 <naufalopang1928@gmail.com>
2025-10-30 12:26:22 +0700

    menambahkan divider baru antara button submit dan elevator card

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -96,7 +96,16 @@ fun FormDataDiri(modifier: Modifier
                 alamat=textAlamat
             }
         ) {
-            Text(stringResource("Submit"))
+            Text(stringResource(R.string.submit))
         }
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
+
+
     }
 }
